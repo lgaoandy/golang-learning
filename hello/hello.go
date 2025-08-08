c56e31668370d58be3593e6b2a4e91a1d69197d3
Andy Gao <andygao@Andys-Laptop.local>
2025-08-08 16:15:57 -0600

    added rsc.io quote example

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// go mod tidy - locates and downloads rsc.io/quote module from import
+import "rsc.io/quote"
+
 func main() {
     fmt.Println("Hello, World!")
+    fmt.Println(quote.Go())
 }
